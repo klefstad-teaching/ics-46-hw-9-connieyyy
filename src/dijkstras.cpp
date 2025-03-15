8a471825c57199a86c9421dd6bf9d595f627a231
Connie Yang <connieay@uci.edu>
2025-03-15 02:40:16 +0000

    added new line after for loop

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -21,6 +21,7 @@ void print_path(const vector<int>& v, int total) {
     for (int i = 0; i < v.size(); ++i) {
         cout << v[i] << " ";
     }
+    cout << "\n"
     cout << "Total cost is " << total << endl;
 }
 
